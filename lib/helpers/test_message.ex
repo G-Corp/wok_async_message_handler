3d4d648b651d7ff61bdfaad3a46660b2f191384e
glejeune <gregoire.lejeune@free.fr>
2016-10-20 19:30:46 +0200

    Update test_message helpers

--- lib/helpers/test_message.ex
+++ lib/helpers/test_message.ex
@@ -1,49 +1,13 @@
 defmodule WokAsyncMessageHandler.Helpers.TestMessage do
-  require Record
-
-  Record.defrecord(:message_transfert,
-    Record.extract(
-      :message_transfert,
-      from_lib: "wok_message_handler/include/wok_message_handler.hrl"
-      )
-    )
-
-  Record.defrecord(:wok_msg,
-    Record.extract(
-      :wok_msg,
-      from_lib: "wok_message_handler/include/wok_message_handler.hrl"
-      )
-    )
-
-  Record.defrecord(:message,
-   Record.extract(
-     :message,
-     from_lib: "wok_message_handler/include/wok_message_handler.hrl"
-     )
-   )
-
   def build_event_message(payload, from, message_id, options \\ []) do
-    message_transfert(
-      message: build_message(payload, from, message_id, options),
-      partition: Keyword.get(options, :partition, 1),
-      topic: Keyword.get(options, :topic, "topic"),
-    )
+    :wok_message.build_event_message(payload,
+                                     from,
+                                     message_id, 
+                                     &__MODULE__.build_message_body/2,
+                                     options)
   end
 
-  defp build_message(payload, from, message_id, options) do
-    wok_msg(
-      message: message(
-        uuid: Ecto.UUID.generate,
-        to: "to_bot",
-        from: from,
-        params: %{},
-        headers: %{message_id: message_id},
-        body: build_message_body(payload, options)
-      )
-    )
-  end
-
-  defp build_message_body(payload, options) do
+  def build_message_body(payload, options) do
     body = %{version: Keyword.get(options, :version, 1), payload: payload}
     metadata = Keyword.get(options, :metadata, nil)
     unless is_nil(metadata), do: body = Map.put(body, :metadata, metadata)
--- spec/helpers/test_message_spec.exs
+++ spec/helpers/test_message_spec.exs
@@ -10,13 +10,15 @@ defmodule WokAsyncMessageHandler.Helpers.TestMessageSpec do
             "to",
             %{version: 1, payload: %{k: "v"}}
            ))
-           .to eq("g2gGZAAHd29rX21zZ2gHZAAHbWVzc2FnZWQACXVuZGVmaW5"
-            <> "lZGQACXVuZGVmaW5lZGQACXVuZGVmaW5lZHQAAAAAZAAJdW5kZ"
-            <> "WZpbmVkZAAJdW5kZWZpbmVkaAZkAAx3b2tfbXNnX3Jlc3BkAAR0"
-            <> "cnVlbQAAAARmcm9tbQAAAAJ0b20AAAAFdG9waWNtAAAAI1t7InZl"
-            <> "cnNpb24iOjEsInBheWxvYWQiOnsiayI6InYifX1dZAAJdW5kZWZp"
-            <> "bmVkZAAJdW5kZWZpbmVkZAAJdW5kZWZpbmVk")
-
+           .to eq("g2gIZAALd29rX21lc3NhZ2VoDGQAA21zZ2QACXVuZGVmaW5lZGQACXVuZGVm"
+                  <> "aW5lZGQACXVuZGVmaW5lZGQACXVuZGVmaW5lZGQACXVuZGVmaW5lZHQAA"
+                  <> "AAAZAAJdW5kZWZpbmVkZAAJdW5kZWZpbmVkZAAJdW5kZWZpbmVkZAAJdW"
+                  <> "5kZWZpbmVkZAAJdW5kZWZpbmVkaAxkAANtc2dkAAl1bmRlZmluZWRtAAA"
+                  <> "ABGZyb21tAAAAAnRvZAAJdW5kZWZpbmVkbQAAACNbeyJ2ZXJzaW9uIjox"
+                  <> "LCJwYXlsb2FkIjp7ImsiOiJ2In19XXQAAAAAZAAJdW5kZWZpbmVkZAAJd"
+                  <> "W5kZWZpbmVkZAAJdW5kZWZpbmVkbQAAAAV0b3BpY2QACXVuZGVmaW5lZG"
+                  <> "QACXVuZGVmaW5lZGQACXVuZGVmaW5lZHQAAAAAZAAEdHJ1ZWQACXVuZGV"
+                  <> "maW5lZA==")
   end
 
   describe "#build_event_message" do
@@ -24,32 +26,41 @@ defmodule WokAsyncMessageHandler.Helpers.TestMessageSpec do
     let :payload, do: %{"name" => "bob", "age" => 69}
     let :from, do: "from_bot"
 
-    before do: allow(Ecto.UUID).to accept(:generate, fn() -> "6a79129d-0990-46cb-9b89-893c48bf2173" end)
+    before do
+      allow(:uuid).to accept(:uuid4, fn() -> "6a79129d-0990-46cb-9b89-893c48bf2173" end)
+      allow(:uuid).to accept(:to_string, fn(x) -> x end)
+    end
 
     context "when no options" do
-      let :expected_message, do: {
-      :message_transfert, :undefined, {
-        :wok_msg, {
-          :message,
-          "6a79129d-0990-46cb-9b89-893c48bf2173",
-          "to_bot",
-          "from_bot",
-          %{},
-          %{message_id: 23},
-          "[{\"version\":1,\"payload\":{\"name\":\"bob\",\"age\":69}}]"
-        },
-        {:wok_msg_resp, false, :undefined, :undefined, :undefined, "" },
-        :undefined, :undefined, :undefined
-      },
-      1,
-      "topic",
-      :undefined,
-      :undefined,
-      :undefined,
-      :undefined,
-      :undefined,
-      :undefined
-    }
+      let :expected_message, do: 
+        {:wok_message, 
+         {:msg, 
+          "6a79129d-0990-46cb-9b89-893c48bf2173", 
+          "from_bot", 
+          "to_bot", 
+          %{message_id: 23}, 
+          "[{\"version\":1,\"payload\":{\"name\":\"bob\",\"age\":69}}]", 
+          %{}, 
+          0, 
+          :undefined, 
+          %{"age" => 69, "name" => "bob"}, "topic", 1}, 
+        {:msg, 
+         :undefined, 
+         :undefined, 
+         :undefined, 
+         :undefined, 
+         :undefined, 
+         %{}, 
+         :undefined, 
+         :undefined, 
+         :undefined, 
+         :undefined, 
+         :undefined}, 
+        :undefined, 
+        :undefined, 
+        %{}, 
+        false, 
+        :undefined}
 
       it do: expect(described_module.build_event_message(
             payload,
@@ -59,29 +70,37 @@ defmodule WokAsyncMessageHandler.Helpers.TestMessageSpec do
     end
 
     context "when options" do
-      let :expected_message, do: {
-      :message_transfert, :undefined, {
-        :wok_msg, {
-          :message,
-          "6a79129d-0990-46cb-9b89-893c48bf2173",
-          "to_bot",
-          "from_bot",
-          %{},
-          %{message_id: 23},
-          "[{\"version\":2,\"payload\":{\"name\":\"bob\",\"age\":69},\"metadata\":{\"seb_error\":true}}]"
-        },
-        {:wok_msg_resp, false, :undefined, :undefined, :undefined, "" },
-        :undefined, :undefined, :undefined
-      },
-      999,
-      "my_topic",
-      :undefined,
-      :undefined,
-      :undefined,
-      :undefined,
-      :undefined,
-      :undefined
-    }
+      let :expected_message, do: 
+        {:wok_message, 
+         {:msg, 
+          "6a79129d-0990-46cb-9b89-893c48bf2173", 
+          "from_bot", 
+          "to_bot", 
+          %{message_id: 23}, 
+          "[{\"version\":2,\"payload\":{\"name\":\"bob\",\"age\":69},\"metadata\":{\"seb_error\":true}}]", 
+          %{}, 
+          0, 
+          :undefined, 
+          %{"age" => 69, "name" => "bob"}, 
+          "my_topic", 
+          999}, 
+        {:msg, 
+         :undefined, 
+         :undefined, 
+         :undefined, 
+         :undefined, 
+         :undefined, 
+         %{}, 
+         :undefined, 
+         :undefined, 
+         :undefined, 
+         :undefined, 
+         :undefined}, 
+        :undefined, 
+        :undefined, 
+        %{}, 
+        false, 
+        :undefined}
       it do: expect(described_module.build_event_message(
             payload,
             from,
